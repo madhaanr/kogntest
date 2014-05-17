cad75aec445620c489a56810d864faefa7514c9d
madhaanr <marko.haanranta@helsinki.fi>
2014-05-17 15:23:02 +0300

    points are count

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -1,54 +1,71 @@
 
-var texts = ["Welcome to Short Term Memory Test, press enter to continue",
-             "You are about to start a short term memory test, press space bar to start",
-             "Input the numbers in order which they came, after you are ready press space bar",
-             "Numbers you remembered"];
+var texts = {welcome:   "Welcome to Short Term Memory Test, press enter to continue",
+             start:     "You are about to start a short term memory test, press space bar to start",
+             input:     "Input the numbers in order which they came, after you are ready press space bar",
+             inputSeq:  "Number sequence as you remembered them",
+             testSeq:   "Number sequence as it was in the test",
+             points:    "points for your results: "};
 var numbers = ["7","6","5","4","3"]
-var numbersRemembered = [ ];
-var index=0;
+var numbersRemembered = [];
+var index = 0;
 var state = 0;
-var interval;
+var points = 0;
+var intervalVariable;
 
 function showNumberInterval() {
-    interval=setInterval(showNumber, 1000);
+    intervalVariable=setInterval(showNumber, 100);
 }
 
 function showNumber() {
     if(index<numbers.length) {
-        document.getElementById("number").innerHTML = numbers[index];
-        console.log("numbers index: "+index);
+        $("#number").html(numbers[index]);
     }
     if(index==numbers.length) {
-        window.clearInterval(interval);
-        document.getElementById("number").innerHTML = "";
-        document.getElementById("info").innerHTML = texts[2];
+        window.clearInterval(intervalVariable);
+        $("#number").html("");
+        $("#info").html(texts['input']);
         state++;
     }
     index++;
 }
 
+function countPoints() {
+    var length=0;
+    if(numbers.length>=numbersRemembered.length) {
+        length=numbers.length;
+    } else {
+        length=numbersRemembered.length;
+    }
+    for(var i=0;i<length;++i) {
+        if(numbers[i]==numbersRemembered[i]) {
+            points+=(i+1)*2;
+            console.log("Hello points: "+points)
+        }
+    }
+}
+
 $(document).keydown(function(e) {
     if(e.which == 13&&state==0) { //enter
-        document.getElementById("info").innerHTML = texts[1];
-        console.log("13")
+        $("#info").html(texts['start']);
         state++;
     }
     else if (e.which == 32&&state==1) { //space to start
-        console.log("32");
         showNumberInterval();
     }
     else if(e.which == 8) { //reset with backspace
         index=0;
         state=0;
-        document.getElementById("info").innerHTML = texts[index];
+        $("#info").html(texts['welcome']);
     }
     else if(state==2) {
-        numbersRemembered += String.fromCharCode(e.which) + " ";
-        console.log(e.which);
+        numbersRemembered += String.fromCharCode(e.which);
         if(e.which==32) {
-            document.getElementById("info").innerHTML = texts[3];
-            document.getElementById("number").innerHTML = numbersRemembered;
-            console.log("number remembered: "+numbersRemembered)
+            countPoints();
+            $("#info").html(texts['testSeq']);
+            $("#number").html(numbers);
+            $("#info2").html(texts['inputSeq']);
+            $("#numbersRemembered").html(numbersRemembered);
+            $("#points").html(texts['points']+points);
         }
     }
     console.log("state: " + state);
@@ -56,6 +73,6 @@ $(document).keydown(function(e) {
 });
 
 $(document).ready(function() {
-   document.getElementById("info").innerHTML = texts[0];
+    $("#info").html(texts['welcome']);
 });
 
